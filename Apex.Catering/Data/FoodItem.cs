f1a6b557f5735b139d681b5cf498b2be3643c127
Shehdy <E4337126@live.tees.ac.uk>
2025-11-03 11:26:12 +0000

    Created entity classes: Defined four entities for the catering application:
    Introduce new classes `FoodBooking`, `FoodItem`, `Menu`, and `MenuFoodItem` in the `Apex.Catering.Data` namespace. These classes represent different entities in the system and include properties with data annotations for primary keys, foreign keys, and validation constraints. This setup is essential for database schema generation and establishing relationships between entities.

---
+++ Apex.Catering/Data/FoodItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Apex.Catering.Data
+{
+    public class FoodItem
+    {
+        [Key]
+        public int FoodItemId { get; set; } // Primary key
+
+
+        [MaxLength(50)]
+        public required string Description { get; set; } // Nullable
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ Apex.Catering/Data/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Apex.Catering.Data
+{
+    public class Menu
+    {
+        [Key]
+        // Since the key name (MenuCode) does not include
+        // "Id", we have to use an annotation (could also
+        // specify this using FluitAPI)
+        [MaxLength(15)]
+        public required string MenuId { get; set; }
+
+        
+       
+        [MaxLength(30)]
+        public string? MenuName { get; set; }
+    }
+}
