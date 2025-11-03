29cb7b327bbd8f3e1dab1b9284d137fc407ff10a
Shehdy <E4337126@live.tees.ac.uk>
2025-11-03 11:26:12 +0000

    Added CateringDbContext and Seeded Initial Data
    This update introduces key entity classes for our catering application:
    
    FoodItem: Contains details about the food items available.
    
    These classes are defined in the Apex.Catering.Data namespace and include properties with data annotations for primary keys, foreign keys, and validation constraints. This setup is crucial for generating the database schema and establishing the relationships between entities.

---
+++ Apex.Catering/Data/MenuFoodItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Apex.Catering.Data
+{
+    public class MenuFoodItem
+    {
+        public int MenuId { get; set; } // Forigen key
+
+        public int FoodItemId { get; set; } // Forigen key
+
+        //Navigation properties
+        public virtual Menu? Menu { get; set; }
+        public virtual FoodItem FoodItem { get; set; }
+    }
+}
