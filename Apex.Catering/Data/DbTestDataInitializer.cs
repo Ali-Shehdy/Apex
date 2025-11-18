8c39faf9394e3152e9e1ffd685cd1198e3b47d5f
Shehdy <E4337126@live.tees.ac.uk>
2025-11-18 02:33:56 +0000

    Add Menu and MenuFoodItem seeding and API controllers
    Introduced data seeding for `Menu` and `MenuFoodItem` entities in `DbTestDataInitializer.cs`, including new `Menu` entries and their associations with `FoodItem` entities. Refactored data initialization to ensure proper sequencing.
    
    Added `MenuFoodItemsController` and `MenusController` to provide RESTful APIs for managing `MenuFoodItem` and `Menu` entities, respectively. Implemented CRUD operations and helper methods for existence checks.
    
    Included necessary `using` directives and improved API structure and consistency by following best practices and adding comments for overposting protection.

--- Apex.Catering/Data/DbTestDataInitializer.cs
+++ Apex.Catering/Data/DbTestDataInitializer.cs
@@ -29,6 +29,9 @@
 
             };
 
+            _context.FoodItems.AddRange(foodItems);
+            _context.SaveChanges();
+
             var foodBookings = new List<FoodBooking>
             {
                 new FoodBooking { FoodBookingId = 2, ClintReferenceId = 1007, NumberOfGuests = 30, MenuId = 2 },
@@ -37,10 +40,40 @@
                 new FoodBooking { FoodBookingId = 5, ClintReferenceId = 1010, NumberOfGuests = 25, MenuId = 2 },
                 };
 
-            
+            _context.FoodBookings.AddRange(foodBookings);
+            _context.SaveChanges(); _context.SaveChanges();
 
-            _context.FoodItems.AddRange(foodItems);
+            var menu = new List<Menu>
+            {
+
+                new Menu { MenuId = 4, MenuName = "Italian Feast" },
+                new Menu { MenuId = 5, MenuName = "Barbecue Bonanza" },
+                new Menu { MenuId = 6, MenuName = "Healthy Options" },
+            };
+            _context.Menus.AddRange(menu);
             _context.SaveChanges();
+
+            var menuFoodItem = new List<MenuFoodItem>
+            {
+                new MenuFoodItem { MenuId = 1, FoodItemId = 1 },
+                new MenuFoodItem { MenuId = 1, FoodItemId = 2 },
+                new MenuFoodItem { MenuId = 1, FoodItemId = 4 },
+                new MenuFoodItem { MenuId = 1, FoodItemId = 5 },
+                // Vegetarian Delight Menu
+                new MenuFoodItem { MenuId = 2, FoodItemId = 3 },
+                new MenuFoodItem { MenuId = 2, FoodItemId = 4 },
+                new MenuFoodItem { MenuId = 2, FoodItemId = 6 },
+                new MenuFoodItem { MenuId = 2, FoodItemId = 8 },
+                // Seafood Special Menu
+                new MenuFoodItem { MenuId = 3, FoodItemId = 5 },
+                new MenuFoodItem { MenuId = 3, FoodItemId = 7 },
+                new MenuFoodItem { MenuId = 3, FoodItemId = 4 }
+            };
+
+            _context.MenuFoodItems.AddRange(menuFoodItem);
+            _context.SaveChanges();
+
+
 
         }
 }
