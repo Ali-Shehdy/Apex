bbc3022425e7d519121ce5816393f00f16148592
Ali-Shehdy <E4337126@live.tees.ac.uk>
2026-01-02 01:09:57 +0000

    Add display for events missing first aiders
    Added a new section in `Index.cshtml` to show upcoming events without a first aider. This includes a conditional block to display either a success alert or a responsive table with event details and action buttons.
    
    Updated `Index.cshtml.cs` to support this feature:
    - Added `UpcomingEventsWithoutFirstAider` property.
    - Modified `OnGetAsync` to fetch and filter events missing first aiders.
    - Included `System` and `System.Linq` namespaces for date handling and LINQ queries.
    
    Fixed a minor formatting issue in `Index.cshtml` by removing an extraneous closing brace.

--- Apex.Events/Pages/EventsList/Index.cshtml.cs
+++ Apex.Events/Pages/EventsList/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -16,13 +18,25 @@ namespace Apex.Events.Pages.EventsList
         }
 
         public IList<Event> Events { get; private set; } = new List<Event>();
+        public IList<Event> UpcomingEventsWithoutFirstAider { get; private set; } = new List<Event>();
 
         public async Task OnGetAsync()
         {
-            Events = await _context.Events
+            var events = await _context.Events
+                .Include(e => e.Staffings)
+                .ThenInclude(s => s.Staff)
                 .AsNoTracking()
                 .OrderBy(e => e.EventDate)
                 .ToListAsync();
+
+            Events = events;
+
+            var today = DateTime.Today;
+            UpcomingEventsWithoutFirstAider = events
+                .Where(e => e.EventDate.Date >= today)
+                .Where(e => !e.Staffings.Any())
+                .OrderBy(e => e.EventDate)
+                .ToList();
         }
     }
 }
